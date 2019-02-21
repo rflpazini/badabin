64f47555a1df85135e866ceaeffb8059e791340a
Rafael Pazini <rflpazini@gmail.com>
2019-02-21 10:13:39 -0300

    fix: change test to work with webflux

--- src/test/java/com/rflpazini/badabin/router/HelloRouterTest.java
+++ src/test/java/com/rflpazini/badabin/router/HelloRouterTest.java
@@ -1,60 +1,46 @@
 package com.rflpazini.badabin.router;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
-import com.rflpazini.badabin.BadabinApplication;
-import org.junit.Ignore;
+import com.rflpazini.badabin.handler.HelloHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@AutoConfigureWebTestClient
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = BadabinApplication.class, webEnvironment = RANDOM_PORT)
+@WebFluxTest(controllers = {HelloHandler.class, HelloRouter.class})
 public class HelloRouterTest {
 
   @Autowired WebTestClient webTestClient;
 
   @Test
-  @Ignore
   public void testExampleWithParameter() {
+
     this.webTestClient
         .get()
-        .uri("/badadin?name=Rafael")
+        .uri("/badabin?name=Rafael")
         .exchange()
         .expectStatus()
         .isOk()
-        .expectBody(String.class)
-        .isEqualTo("Welcome to a world of surprises! Rafael");
+        .expectBody()
+        .jsonPath("content", "Welcome to a world of surprises! Rafael");
   }
 
   @Test
-  @Ignore
   public void testExampleWithoutParameter() {
     this.webTestClient
         .get()
-        .uri("/badadin")
-        .accept(MediaType.TEXT_PLAIN)
+        .uri("/badabin")
         .exchange()
         .expectStatus()
         .isOk()
-        .expectBody(String.class)
-        .isEqualTo("Welcome to a world of surprises! Stranger...");
+        .expectBody()
+        .jsonPath("content", "Welcome to a world of surprises! Stranger...");
   }
 
   @Test
   public void testExampleWithError() {
-    this.webTestClient
-        .get()
-        .uri("/badadin/something")
-        .accept(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus()
-        .isNotFound();
+    this.webTestClient.get().uri("/badabin/something").exchange().expectStatus().isNotFound();
   }
 }
