ce5358fe4e42898f44fff04da39c27e684534ee4
Rafael Pazini <rflpazini@gmail.com>
2019-02-20 21:54:20 -0300

    refactor: add new lombok plugin and create new routes

--- src/main/java/com/rflpazini/badabin/domain/Greeting.java
+++ src/main/java/com/rflpazini/badabin/domain/Greeting.java
@@ -3,35 +3,16 @@ package com.rflpazini.badabin.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.Setter;
 
+@Builder
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Greeting {
 
   private final long id;
   private final String content;
   private final String apiName;
   private final String version;
-
-  public Greeting(long id, String content, String apiName, String version) {
-    this.id = id;
-    this.content = content;
-    this.apiName = apiName;
-    this.version = version;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public String getApiName() {
-    return apiName;
-  }
-
-  public String getVersion() {
-    return version;
-  }
 }
