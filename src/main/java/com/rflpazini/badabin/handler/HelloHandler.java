05b5f18830b2a613b7737e60d1b01ce747f2e7d2
Rafael Pazini <rflpazini@gmail.com>
2019-03-21 13:37:24 -0300

    refactor: fix error reported by sonarlint and change version name

--- src/main/java/com/rflpazini/badabin/handler/HelloHandler.java
+++ src/main/java/com/rflpazini/badabin/handler/HelloHandler.java
@@ -14,14 +14,15 @@ import reactor.core.publisher.Mono;
 @Component
 public class HelloHandler {
 
-  private static final String template = "Welcome to a world of surprises, %s";
+  private static final String TEMPLATE_MESSAGE = "Welcome to a world of surprises, %s";
   private final AtomicLong counter = new AtomicLong();
 
   public Mono<ServerResponse> sayHello(ServerRequest request) {
     String name = request.queryParam("name").orElse("Stranger...");
 
     Greeting welcomeGreeting =
-        new Greeting(counter.incrementAndGet(), String.format(template, name), "Badadin", "v0.1");
+        new Greeting(
+            counter.incrementAndGet(), String.format(TEMPLATE_MESSAGE, name), "Badadin", "v0.1");
 
     return ok().contentType(MediaType.APPLICATION_JSON)
         .body(BodyInserters.fromObject(welcomeGreeting));
