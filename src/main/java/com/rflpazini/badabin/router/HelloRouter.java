e0791aa4f3b62ddd6ad72beb22ac31e38297e3e4
Rafael Pazini <rflpazini@gmail.com>
2019-03-27 20:25:05 -0300

    fix: add @getter again to fix error when create json

--- src/main/java/com/rflpazini/badabin/router/HelloRouter.java
+++ src/main/java/com/rflpazini/badabin/router/HelloRouter.java
@@ -1,14 +1,10 @@
 package com.rflpazini.badabin.router;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 import com.rflpazini.badabin.handler.HelloHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -19,11 +15,6 @@ public class HelloRouter {
   @Bean
   public RouterFunction<ServerResponse> helloRoute(HelloHandler helloHandler) {
     return RouterFunctions.route(
-            GET("/badabin").and(accept(MediaType.APPLICATION_JSON)), helloHandler::sayHello)
-        .andRoute(
-            POST("/people")
-                .and(accept(MediaType.APPLICATION_JSON))
-                .and(RequestPredicates.contentType(MediaType.APPLICATION_JSON)),
-            helloHandler::sayHello);
+            GET("/badabin"), helloHandler::sayHello);
   }
 }
