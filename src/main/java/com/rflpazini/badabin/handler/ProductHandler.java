634b9a40c6bf5a9f7f40171b1776389afb3e35d5
Rafael Pazini <rflpazini@gmail.com>
2019-03-28 00:10:40 -0300

    refactor: add new endpoints

--- src/main/java/com/rflpazini/badabin/handler/ProductHandler.java
+++ src/main/java/com/rflpazini/badabin/handler/ProductHandler.java
@@ -4,6 +4,8 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 
 import com.rflpazini.badabin.domain.Product;
 import com.rflpazini.badabin.service.ProductService;
+import java.util.stream.Stream;
+import javax.swing.text.Document;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -21,6 +23,23 @@ public class ProductHandler {
 
   public Mono<ServerResponse> getAllProducts(ServerRequest request) {
     Flux<Product> allProducts = mService.findAll();
-    return ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromObject(allProducts));
+    return ok().contentType(MediaType.APPLICATION_JSON_UTF8).body(allProducts, Product.class);
+  }
+
+  public Mono<ServerResponse> getById(ServerRequest request) {
+    long id = Long.parseLong(request.pathVariable("id"));
+
+    return ok().contentType(MediaType.APPLICATION_JSON_UTF8)
+        .body(mService.findById(id), Product.class);
+  }
+
+  public Mono<ServerResponse> create(ServerRequest request) {
+    return request
+        .bodyToMono(Product.class)
+        .flatMap(mService::create)
+        .flatMap(
+            product ->
+                ok().contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .body(BodyInserters.fromObject(product)));
   }
 }
--- src/main/java/com/rflpazini/badabin/router/ProductRouter.java
+++ src/main/java/com/rflpazini/badabin/router/ProductRouter.java
@@ -1,14 +1,15 @@
 package com.rflpazini.badabin.router;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 import com.rflpazini.badabin.handler.ProductHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
@@ -16,7 +17,8 @@ public class ProductRouter {
 
   @Bean
   public RouterFunction<ServerResponse> productRoute(ProductHandler productHandler) {
-    return RouterFunctions.route(
-        GET("/products").and(accept(MediaType.APPLICATION_JSON)), productHandler::getAllProducts);
+    return route(GET("/product"), productHandler::getAllProducts)
+        .andRoute(GET("/product/{id}"), productHandler::getById)
+        .andRoute(POST("/product").and(accept(APPLICATION_JSON)), productHandler::create);
   }
 }
