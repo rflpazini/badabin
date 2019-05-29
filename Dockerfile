f5f11e2738eac11683b8ade0023b0a8e902a4531
Rafael Pazini <rflpazini@gmail.com>
2019-05-29 10:39:57 -0300

    add: enable graal options on JVM

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ VOLUME /tmp
 MAINTAINER rflpazini
 ADD build/libs/*.jar app.jar
 
-ENV JAVA_OPTS=""
+ENV JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"
+ENV GRAAL_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler -Dgraal.PrintCompilation=true"
 ENV SPRING_PROFILE="prod"
 
 EXPOSE 8080
 
-ENTRYPOINT exec java $JAVA_OPTS \
- -Djava.security.egd=file:/dev/./urandom \
+ENTRYPOINT exec java $JAVA_OPTS $GRAAL_OPTS\
  -Dspring.profiles.active=$SPRING_PROFILE \
  -jar app.jar
--- src/main/java/com/rflpazini/badabin/handler/HelloHandler.java
+++ src/main/java/com/rflpazini/badabin/handler/HelloHandler.java
@@ -29,8 +29,33 @@ public class HelloHandler {
   }
 
   public Mono<ServerResponse> saySomething(ServerRequest request) {
-    return ok().contentType(MediaType.APPLICATION_JSON)
-      .body(BodyInserters.fromObject("UNI-FACEF"));
-  }
+    final int ITERATIONS = Math.max(Integer.getInteger("iterations", 1), 1);
 
+    String sentence = String.join(" ");
+
+    for (int iter = 0; iter < ITERATIONS; iter++) {
+      if (ITERATIONS != 1) {
+        System.out.println("-- iteration " + (iter + 1) + " --");
+      }
+
+      long total = 0, start = System.currentTimeMillis(), last = start;
+
+      for (int i = 1; i < 10_000_000; i++) {
+        total += sentence
+          .chars()
+          .filter(Character::isUpperCase)
+          .count();
+        if (i % 1_000_000 == 0) {
+          long now = System.currentTimeMillis();
+          System.out.printf("%d (%d ms)%n", i / 1_000_000, now - last);
+          last = now;
+        }
+      }
+
+      System.out.printf("total: %d (%d ms)%n", total, System.currentTimeMillis() - start);
+    }
+
+    return ok().contentType(MediaType.APPLICATION_JSON)
+      .body(BodyInserters.fromObject("Confirmed"));
+  }
 }
